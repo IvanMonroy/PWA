cea7622f24f2217b7c3ada479566d029bb099de3
IvanMonroy <ivan.monroy@hrinteractive.co>
2019-11-01 15:29:03 -0500

    Config server

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/lifecycle-hooks'));
+app.use(express.static(__dirname + '/dist/pwa-api'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/lifecycle-hooks/index.html'));
+res.sendFile(path.join(__dirname+'/dist/pwa-api/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
